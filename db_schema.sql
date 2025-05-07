66ca26a18c2f2166f3917881c73cffdb7dd0d275
Deepkiran Kaur <140378743+deepkiran-k@users.noreply.github.com>
2025-05-07 23:56:01 +0530

    Create db_schema.sql

---
+++ db_schema.sql
@@ -0,0 +1,16 @@
+-- Database: `fakenews`
+
+-- --------------------------------------------------------
+
+-- Table structure for table `search_results`
+
+CREATE TABLE `search_results` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `search_query` varchar(255) NOT NULL,
+  `verdict` enum('True','False','Inconclusive') NOT NULL,
+  `explanation` text NOT NULL,
+  `relevant_links` text,
+  `search_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
